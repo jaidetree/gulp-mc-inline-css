2ab86415a3072b4d7c47734ffebaac1f26c9194a
Jay Zawrotny <jayzawrotny@gmail.com>
2014-02-13 12:12:34 -0500

    Stuff is happening now I guess?

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 var through = require('through2');
 var gutil = require('gulp-util');
+var MailChimpAPI = require('mailchimp').MailChimpAPI;
 var PluginError = gutil.PluginError;
+var api;
 
 // Consts
 const PLUGIN_NAME = 'gulp-inline-css';
@@ -11,12 +13,21 @@ function prefixStream(prefixText) {
   return stream;
 }
 
+
 // Plugin level function (dealing with files)
-function gulpInlineCSS(apikey, opts) {
+function gulpInlineCSS(apikey, opt) {
   if (!apikey) {
-    throw PluginError(PLUGIN_NAME, "Missing MailChimp API Key");
+    throw new PluginError(PLUGIN_NAME, "Missing MailChimp API Key");
   }
-  prefixText = new Buffer(prefixText); // allocate ahead of time
+  if (!opt) opt = {};
+
+  //prefixText = new Buffer(prefixText); // allocate ahead of time
+  
+  try { 
+    api = new MailChimpAPI(apikey, { version : '2.0' });
+  } catch (error) {
+    console.log(error.message);
+  }
 
   // Creating a stream through which each file will pass
   var stream = through.obj(function (file, enc, callback) {
@@ -26,7 +37,15 @@ function gulpInlineCSS(apikey, opts) {
     }
 
     if (file.isBuffer()) {
-      file.contents = Buffer.concat([prefixText, file.contents]);
+      // file.contents = Buffer.concat([file.contents]);
+      console.log( file.contents.toString('utf8') );
+      api.call('helper', 'inline-css', { 'html': file.contents, 'strip_css': true  }, function (error, data) {
+        if (error) {
+          console.log(error.message);
+        } else {
+          console.log(JSON.stringify(data)); // Do something with your data!
+        }
+      });
       this.push(file);
       return callback();
     }
